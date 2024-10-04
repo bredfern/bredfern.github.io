cc7ed8c1d3eb8f5b9a7daf2b46da033ea5b41b40
Brian Redfern <bredfern@goddess.lan>
2024-10-03 17:50:54 -0700

    up

--- contact.js
+++ contact.js
@@ -3,7 +3,7 @@ function handleSubmit(event) {
 
   const status = document.getElementById("contact-form-status");
   const email = document.getElementById("email").value;
-  const message = document.getElementById("message").text;
+  const message = document.getElementById("message").value;
 
   window.location.href = `mailto:brianwredfern@gmail.com?subject=Thesubject${email}&body=${message}}`;
   status.innerHTML = "Thanks for saying hello!";
