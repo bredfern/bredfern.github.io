0cf1a5dbbcd717a08b24f70e6fb87cf06c6ef2dd
Brian Redfern <brianwredfern@gmail.com>
2025-10-12 11:44:57 -0700

    up

--- assets/player.js
+++ assets/player.js
@@ -42,7 +42,7 @@ class AudioPlaylistPlayer extends HTMLElement {
     this.playPauseBtn = this.shadowRoot.querySelector("#play-pause-btn");
     this.canvas = this.shadowRoot.querySelector("#visualizer-canvas");
     this.canvasCtx = this.canvas.getContext("2d");
-    this.canvasCtx.font ="240px, serif";
+    this.canvasCtx.font ="240px, sans-serif";
     this.canvasCtx.fillText("🎶My Compositions", 80, 50);
 
     // Event listeners
