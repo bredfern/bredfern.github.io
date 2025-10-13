084b957f0bf39bfbae55e83839f62176eef0dfb7
Brian Redfern <brianwredfern@gmail.com>
2025-10-12 17:27:56 -0700

    up

--- assets/player.js
+++ assets/player.js
@@ -163,7 +163,7 @@ class AudioPlaylistPlayer extends HTMLElement {
 
 #playlist-list {
     list-style: none;
-    width: 100%;
+    width: 89%;
     padding: 20px;
     max-height: 200px;
     overflow-y: auto;
