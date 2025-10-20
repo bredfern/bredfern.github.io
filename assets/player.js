76167e27da9fbb61aee456f354d438e6f1c224ee
Brian Redfern <brianwredfern@gmail.com>
2025-10-20 14:09:05 -0700

    up

--- assets/player.js
+++ assets/player.js
@@ -124,16 +124,19 @@ class AudioPlaylistPlayer extends HTMLElement {
           display: block;
           font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
           padding: 6px;
+          padding-right: 19px;
   max-width: 640px; /* Increased max-width for repos */
-  margin: 20px auto;
+  margin: 6px auto;
   margin-top: 0;
   box-shadow: 0 3px 10px rgba(0, 0, 0, 0.05);
   background-color: #029356;
   border-radius: 8px;
+  text-align: center'
 }
 
 .player-container {
     padding: 0;
+    padding-left: 20px;
     margin-top: 0;
     width: 100%;
     margin-left: auto;
@@ -141,8 +144,8 @@ class AudioPlaylistPlayer extends HTMLElement {
 }
 
 #visualizer-canvas {
-    width: 100%;
     border-radius: 4px;
+    width: 100%;
     display: block;
     margin-bottom: 14px;
     margin-top: 0;
