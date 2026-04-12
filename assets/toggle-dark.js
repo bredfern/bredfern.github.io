36989a0c2a82cc2fdb0b9340783bb4fbd3395811
Brian Redfern <brianwredfern@gmail.com>
2026-04-11 17:35:01 -0700

    up

--- assets/toggle-dark.js
+++ assets/toggle-dark.js
@@ -3,4 +3,9 @@ const btn = document.getElementById('btn-toggle');
 btn.addEventListener("click", function() {
   const element = document.body;
   element.classList.toggle("dark-mode");
+  if (btn.innerHTML === '🌛') {
+    btn.innerHTML = '🌞';
+  } else {
+    btn.innerHTML = '🌛';
+  }
 });
