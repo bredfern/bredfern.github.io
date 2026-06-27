2eac1d0ff7904f0b0db55c7b5771619f10dd25cf
Brian Redfern <brianwredfern@gmail.com>
2026-06-27 12:48:43 -0700

    update

--- assets/js/github-user-profile.js
+++ assets/js/github-user-profile.js
@@ -131,7 +131,7 @@ class GithubUserProfile extends HTMLElement {
             <style>
                 :host {
                     display: block;
-                    font-family: 'cloude-outline', serif;
+                    font-family: 'Scaver Med', serif;
                     padding: 1.5rem;
                     max-width: 40rem;
                     margin: 1.25rem auto;
